451a7c39ef0a7d5b239d004e87de3e8aad2ec78d
pel1780 <118698633+pel1780@users.noreply.github.com>
2023-01-03 17:50:41 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,5 +5,17 @@ $(function () {
         fade: true,
         pauseOnHover: false,
         pauseOnFocus: false,
-    });
+    }); //메인 슬라이드
+
+    $('.tab_slide').slick({
+        arrows: false,
+    }); // 탭 슬라이드
+
+    $('.tab_concept_menu ul>li button').on('click', function () {
+        var idx = $(this).parent().index();
+        console.log($(this).parent().index());
+        $('.tab_concept_slide ul>li').removeClass('on');
+        $('.tab_concept_slide ul>li').eq(idx).addClass('on');
+
+    })
 })
