3071b35ff7cb387c40cc709981e12443ccbb895f
pel1780 <118698633+pel1780@users.noreply.github.com>
2023-01-05 17:33:09 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -11,16 +11,21 @@ $(function () {
         pauseOnFocus: false,
     }); //메인 슬라이드
 
+    $('.scroll_down a').on('click', function (event) {
+        event.preventDefault();
+        $('html, body').animate({ scrollTop: $(this.hash).offset().top }, 1000);
+    })
+
     $('.tab_slide').slick({
         arrows: false,
     });
 
     $('.mainConcept .controller li:nth-child(1)').on('click', function () {
         $('.tab_concept_slide ul>li.on .tab_slide').slick('slickPrev');
-    })
+    });
     $('.mainConcept .controller li:nth-child(2)').on('click', function () {
         $('.tab_concept_slide ul>li.on .tab_slide').slick('slickNext');
-    })
+    });
     // 탭 슬라이드
 
     $('.tab_concept_menu ul>li button').on('click', function () {
@@ -31,5 +36,20 @@ $(function () {
 
         $('.tab_concept_menu ul>li').removeClass('on');
         $(this).parent().addClass('on');
-    })
-})
+    });
+    // 메뉴버튼 클릭
+
+    $(window).on('scroll', function () {
+        var sct = $(window).scrollTop();
+        if (sct > 1000) {
+            $('.to_top').addClass('on');
+        } else {
+            $('.to_top').removeClass('on');
+        }
+    });
+
+    $('.to_top').on('click', function () {
+        $('html, body').stop().animate({ scrollTop: 0 }, 1000)
+    });
+    // totop button 작동
+});
